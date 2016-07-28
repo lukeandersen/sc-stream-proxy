1fde8eb5c9dfe85a982c4c8ccc40d00bd97699a6
Luke Andersen <luke@lawebdesign.com.au>
2016-07-29 00:22:30 +1000

    Add port

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ var http = require('http');
 var url = require('url');
 var request = require('request');
 
-http.createServer(onRequest).listen(3000);
+var port = process.env.PORT || 3000;
+
+http.createServer(onRequest).listen(port);
 
 function onRequest(req, res) {
 
